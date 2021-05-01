9cffec3f62d11559952cfdaf25e33a8830f98b74
Brian Rice <brianrice013@gmail.com>
2021-04-30 23:13:09 -0500

    Initialze Flask app for pitchfork app

--- app.py
+++ app.py
@@ -18,51 +18,69 @@ logger.debug("Web app log")
 # Initialize the database session
 from src.add_albums import Albums, AlbumManager
 
-track_manager = AlbumManager(app)
+album_manager = AlbumManager(app)
 
 
 @app.route("/")
 def index():
-    """Main view that lists songs in the database.
+    """
+    Main view that lists albums in the database.
 
-    Create view into index page that uses data queried from Track database and
-    inserts it into the msiapp/templates/index.html template.
+    Create view into index page that uses data queried from `albums` database and
+    inserts it into the app/templates/index.html template.
 
-    Returns: rendered html template
-
+    Returns:
+        Rendered HTML template
     """
 
     try:
         albums = (
-            track_manager.session.query(Albums).limit(app.config["MAX_ROWS_SHOW"]).all()
+            album_manager.session.query(Albums).limit(app.config["MAX_ROWS_SHOW"]).all()
         )
         logger.debug("Index page accessed")
         return render_template("index.html", albums=albums)
     except:
         traceback.print_exc()
-        logger.warning("Not able to display albums, error page returned")
+        logger.warning("Not able to display albums. Error page returned.")
         return render_template("error.html")
 
 
 @app.route("/add", methods=["POST"])
 def add_entry():
-    """View that process a POST with new song input
+    """
+    View that process a POST with new album input.
 
-    :return: redirect to index page
+    Returns:
+        Redirect to index page
     """
 
     try:
-        track_manager.add_track(
-            artist=request.form["artist"],
+        album_manager.add_album(
             album=request.form["album"],
-            title=request.form["title"],
+            artist=request.form["artist"],
+            reviewauthor=request.form["reviewauthor"],
+            score=request.form["score"],
+            releaseyear=request.form["releaseyear"],
+            reviewdate=request.form["reviewdate"],
+            recordlabel=request.form["recordlabel"],
+            genre=request.form["genre"],
+            danceability=request.form["danceability"],
+            energy=request.form["energy"],
+            key=request.form["key"],
+            loudness=request.form["loudness"],
+            speechiness=request.form["speechiness"],
+            acousticness=request.form["acousticness"],
+            instrumentalness=request.form["instrumentalness"],
+            liveness=request.form["liveness"],
+            valence=request.form["valence"],
+            tempo=request.form["tempo"],
         )
         logger.info(
-            "New song added: %s by %s", request.form["title"], request.form["artist"]
+            "New album added: %s by %s", request.form["album"], request.form["artist"]
         )
         return redirect(url_for("index"))
     except:
-        logger.warning("Not able to display albums, error page returned")
+        logger.warning("Not able to display albums. Error page returned.")
         return render_template("error.html")
 
 
