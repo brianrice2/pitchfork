62e464a5e0558ddaf23d64356713d06661be3874
Brian Rice <brianrice013@gmail.com>
2021-05-07 14:33:36 -0500

    Clarify `parse_s3()` docstrings
    It's pretty clear already which function is being tested,
    so it's unnecessary to repeat in the docstring.

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -4,14 +4,14 @@ from src import load_data
 
 
 def test_parse_s3():
-    """`parse_s3()` correctly splits bucket and path."""
+    """Correctly splits bucket and path."""
     bucket, path = load_data.parse_s3("s3://my-bucket/my-path")
     assert bucket == "my-bucket"
     assert path == "my-path"
 
 
 def test_parse_s3_long_path():
-    """`parse_s3()` correctly splits bucket at the first "/"."""
+    """Correctly splits bucket at the first "/"."""
     bucket, path = load_data.parse_s3("s3://my-bucket/my/super/long/path")
     assert bucket == "my-bucket"
     assert path == "my/super/long/path"
