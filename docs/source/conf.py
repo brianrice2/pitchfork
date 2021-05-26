037ede1cb6f373c38a51e752ecf31fe7124dfc4d
Brian Rice <brianrice013@gmail.com>
2021-05-25 19:02:58 -0500

    Bump minor version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2020, Chloe Mawer"
 author = "Brian Rice"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1"
+release = "0.2"
 
 
 # -- General configuration ---------------------------------------------------
