92f9717121dd505304782c144771bc352430f463
Brian Rice <brianrice013@gmail.com>
2021-06-01 20:27:06 -0500

    Emphase return values not important in `test_parse_s3`

--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -20,4 +20,4 @@ def test_parse_s3_long_path():
 def test_parse_s3_missing_path():
     """Missing S3 path (only bucket provided)."""
     with pytest.raises(AttributeError):
-        bucket, path = load_data.parse_s3("s3://my-bucket/")
+        _, _ = load_data.parse_s3("s3://my-bucket/")
