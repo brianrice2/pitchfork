f3f8eb4d22b0985b48838134ec7585ee7640e5b0
Brian Rice <brianrice013@gmail.com>
2021-06-04 09:16:35 -0500

    Move input/output locations into their own variables

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
-S3_BUCKET="s3://2021-msia423-rice-brian"
 PIPELINE_CONFIG="config/pipeline.yaml"
+S3_BUCKET="s3://2021-msia423-rice-brian"
+RAW_DATA_PATH="data/raw/P4KxSpotify.csv"
+CLEANED_DATA_PATH="data/cleaned/P4KxSpotify.csv"
+SAVED_MODEL_PATH="models/gbt_pipeline.joblib"
+MODEL_PREDICTIONS_PATH="models/cleaned_with_predictions.csv"
+
 
 python3 run.py pipeline clean \
-  --input "${S3_BUCKET}/data/raw/P4KxSpotify.csv" \
+  --input "${S3_BUCKET}/${RAW_DATA_PATH}" \
   --config "${PIPELINE_CONFIG}" \
-  --output "${S3_BUCKET}/data/cleaned/P4KxSpotify.csv"
+  --output "${S3_BUCKET}/${CLEANED_DATA_PATH}"
 
 python3 run.py pipeline model \
-  --input "${S3_BUCKET}/data/cleaned/P4KxSpotify.csv" \
+  --input "${S3_BUCKET}/${CLEANED_DATA_PATH}" \
   --config "${PIPELINE_CONFIG}" \
-  --output "${S3_BUCKET}/models/gbt_pipeline.joblib"
+  --output "${S3_BUCKET}/${SAVED_MODEL_PATH}"
 
 python3 run.py pipeline predict \
-  --input "${S3_BUCKET}/data/cleaned/P4KxSpotify.csv" \
-  --model "${S3_BUCKET}/models/gbt_pipeline.joblib" \
+  --input "${S3_BUCKET}//${CLEANED_DATA_PATH}" \
+  --model "${S3_BUCKET}/${SAVED_MODEL_PATH}" \
   --config "${PIPELINE_CONFIG}" \
-  --output "${S3_BUCKET}/models/cleaned_with_predictions.csv"
+  --output "${S3_BUCKET}/${MODEL_PREDICTIONS_PATH}"
